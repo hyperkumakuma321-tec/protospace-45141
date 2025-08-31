3f5b63c33769cfd8750f8b1159623de78845fe92
hyperkumakuma321-tec <marbletyokotec@gmail.com>
2025-08-31 18:49:40 +0900

    空コメント投稿　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype 
 
   def create
-    @comment = Comment.create(comment_params)
-
+    @comment = @prototype.comments.new(comment_params.merge(user: current_user))
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
+      @comments = @prototype.comments.includes(:user)
       render "prototypes/show", status: :unprocessable_entity
     end
   end
 
   private
+
+    def set_prototype
+      @prototype = Prototype.find(params[:prototype_id])
+    end
 
     def comment_params
       params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
